7b8fa35803368cbc3829560bbe0d8abacb03fe6c
AkbarNurDzikri <dzikrinurakbar94@gmail.com>
2023-11-16 09:16:16 +0700

    Merubah controller login. Menghilangkan pemeriksaan isVerified. Saat user belum memverifikasi emailnya, tetap bisa login.

--- backend/src/controllers/otentifikasi/loginController.js
+++ backend/src/controllers/otentifikasi/loginController.js
@@ -5,20 +5,16 @@ import tokenGenerator from "../../services/tokens/tokenGenerator.js";
 const login = async (req, res) => {
   const {username, password} = req.body;
   const user = await getKaryawanEksis(username);
-  if(user === null) {
+  if(!user) {
     return res.status(401).json({errors: 'Username atau password salah !'});
   } else {
-    if(user.isVerified === false) return res.status(401).json({errors: 'Maaf, Anda belum verifikasi email. \n Silahkan lakukan verifikasi email terlebih dahulu sebelum melanjutkan.'});
-
     const checkPassword = await verifyPassword(password, user.password);
     if(checkPassword === false) return res.status(401).json({errors: 'Username atau password salah !'});
     
-    const fullname = user.fullname;
-    const token = tokenGenerator({fullname});
-    
+    const username = user.username;
+    const token = tokenGenerator({username});
     return res.status(200).json({token: token});
   }
-  
 }
 
 export default login;
